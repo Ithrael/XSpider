57922f2390faafd5d4c2e8e96380b5350ed1706f
ithrael <wh01096045@gmail.com>
2024-01-20 22:21:49 +0800

    feat(spider): add restriction config

--- main.go
+++ main.go
@@ -24,6 +24,46 @@ type PageDetail struct {
 	Fingerprint  string
 }
 
+var allowedDomains map[string]struct{}
+var excludeDomains map[string]struct{}
+var visitURL string
+var out string
+var maxCount int
+var maxDepth int
+var parallelism int
+var randomDelayMaxTime int
+var headers map[string]string
+
+func init() {
+	flag.StringVar(&visitURL, "url", "https://www.baidu.com", "URL to visit")
+	flag.StringVar(&out, "out", "out.csv", "out file name")
+	flag.Parse()
+
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalf("Fatal error config file: %s", err)
+	}
+
+	randomDelayMaxTime = viper.GetInt("Restriction.RandomDelayMaxTime")
+	parallelism = viper.GetInt("Restriction.Parallelism")
+	allowdomains := viper.GetStringSlice("Restriction.AllowedDomains")
+	excludedomains := viper.GetStringSlice("Restriction.ExcludedDomains")
+	maxCount = viper.GetInt("Restriction.MaxCount")
+	maxDepth = viper.GetInt("Restriction.MaxDepth")
+	headers = viper.GetStringMapString("Headers")
+	allowedDomains = make(map[string]struct{})
+	for _, d := range allowdomains {
+		allowedDomains[d] = struct{}{}
+	}
+
+	excludeDomains = make(map[string]struct{})
+	for _, d := range excludedomains {
+		excludeDomains[d] = struct{}{}
+	}
+}
+
 func isSubdomainOfAllowedDomain(host string, allowedDomains map[string]struct{}) bool {
 	for domain := range allowedDomains {
 		if strings.HasSuffix(host, domain) {
@@ -97,7 +137,10 @@ func parseResp(resp *colly.Response, url string) (*PageDetail, error) {
 }
 
 func runSpider(detailsCh chan *PageDetail) {
-	c := colly.NewCollector()
+	var count int
+	c := colly.NewCollector(
+		colly.MaxDepth(maxDepth),
+	)
 
 	if parallelism != 0 {
 		c.Limit(&colly.LimitRule{Parallelism: parallelism, RandomDelay: time.Duration(randomDelayMaxTime) * time.Second})
@@ -108,6 +151,14 @@ func runSpider(detailsCh chan *PageDetail) {
 	})
 
 	c.OnRequest(func(r *colly.Request) {
+		if maxCount != 0 {
+			if count > maxCount {
+				fmt.Println("Reached max count!")
+				r.Abort()  // 如果超过最大数量，则取消请求
+				os.Exit(1) // 并退出程序
+			}
+		}
+
 		if isExcludedDomain(r.URL.Hostname(), excludeDomains) {
 			r.Abort()
 		}
@@ -115,6 +166,7 @@ func runSpider(detailsCh chan *PageDetail) {
 			r.Abort()
 		} else {
 			log.Println("Visiting", r.URL)
+			count++
 			if len(headers) > 0 {
 				for key, value := range headers {
 					r.Headers.Set(key, value)
@@ -137,42 +189,6 @@ func runSpider(detailsCh chan *PageDetail) {
 		log.Fatalf("Visit URL failed: %v", err)
 	}
 	close(detailsCh)
-}
-
-var allowedDomains map[string]struct{}
-var excludeDomains map[string]struct{}
-var visitURL string
-var out string
-var parallelism int
-var randomDelayMaxTime int
-var headers map[string]string
-
-func init() {
-	flag.StringVar(&visitURL, "url", "https://www.baidu.com", "URL to visit")
-	flag.StringVar(&out, "out", "out.csv", "out file name")
-	flag.Parse()
-
-	viper.SetConfigName("config")
-	viper.AddConfigPath(".")
-	err := viper.ReadInConfig()
-	if err != nil {
-		log.Fatalf("Fatal error config file: %s", err)
-	}
-
-	randomDelayMaxTime = viper.GetInt("RandomDelayMaxTime")
-	parallelism = viper.GetInt("Parallelism")
-	allowdomains := viper.GetStringSlice("AllowedDomains")
-	excludedomains := viper.GetStringSlice("ExcludedDomains")
-	headers = viper.GetStringMapString("Headers")
-	allowedDomains = make(map[string]struct{})
-	for _, d := range allowdomains {
-		allowedDomains[d] = struct{}{}
-	}
-
-	excludeDomains = make(map[string]struct{})
-	for _, d := range excludedomains {
-		excludeDomains[d] = struct{}{}
-	}
 }
 
 func main() {
